8f1d6e631e3be165e5ea19d6d8912550bb80387f
zduan <zduanx@gmail.com>
2017-12-16 15:51:05 -0800

    hw4 basic done

--- week4/executor/executor_server.py
+++ week4/executor/executor_server.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 import json
+import executor_utils as eu
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,8 +19,10 @@ def build_and_run():
     code = data['code']
     lang = data['lang']
     print("API got called with code %s in %s" %(code, lang))
-
-    return jsonify({'build': "asdf", 'run': "asdfasdfasdfasdf"})
+    # return jsonify({'build': "asdf", 'run': "asdfasdfasdfasdf"})
+    result = eu.build_and_run(code, lang)
+    return jsonify(result)
 
 if __name__ == '__main__':
+    eu.load_image()
     app.run(debug=True)
